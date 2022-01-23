7e7a8e177aaf9ec67ebe1c1d590052eee9685dee
richasinhaa <richa.sinha0603@gmail.com>
2022-01-23 20:43:36 +0800

    Blinkit Clone API Template added

---
+++ src/main/java/com/api/blinkitClone/BlinkitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.blinkitClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkitCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkitCloneApplication.class, args);
+	}
+
+}
